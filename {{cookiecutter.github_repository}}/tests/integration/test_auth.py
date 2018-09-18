247cf308bf7411ee1f79da4cd7b7e5bc2036f041
f9e <35344456+f9e@users.noreply.github.com>
2018-09-18 15:03:39 -0400

    Use uncommon password
    Test with password that will pass CommonPasswordValidator

--- {{cookiecutter.github_repository}}/tests/integration/test_auth.py
+++ {{cookiecutter.github_repository}}/tests/integration/test_auth.py
@@ -104,7 +104,7 @@ def test_user_password_reset_and_confirm(client, settings, mocker):
     token = kwargs['context']['token']
 
     # confirm we can reset password using context values
-    new_password = 'paSswOrd2'
+    new_password = 'paSsw0rd2'[::-1] # reversed, i.e. '2dr0wsSap'
     password_reset_confirm_data = {
         'new_password': new_password,
         'token': token
